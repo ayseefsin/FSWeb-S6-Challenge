1f627db6678d34ebf445a83a6abf18f6915761e5
ayseefsin <ayse.efsin@yandex.com>
2024-03-07 07:26:16 +0300

    acordeon done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Karakter } from "./components/Karakter";
+import axios from "axios";
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchPeople = () => {
+    return axios.get("https://swapi.dev/api/people/");
+  };
+  const fetchMovie = () => {
+    return axios.get("https://swapi.dev/api/films/");
+  };
+
+  useEffect(() => {
+    Promise.all([fetchPeople(), fetchMovie()]).then((values) => {
+      setLoading(false);
+      console.log(values[1].data[0].results);
+      setCharacters(values[0].data);
+      setMovies(values[1].data[0].results);
+    });
+  }, []);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <h1 className="text-[5vw] text-[white] mt-[5vw] font-bold ">
+            Karakterler
+          </h1>
+          {characters.map((char) => {
+            return <Karakter char={char} movies={movies} />;
+          })}
+        </>
+      )}
+      ;
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,49 @@
+import { useState } from "react";
+
 // Karakter bileşeniniz buraya gelecek
+export const Karakter = ({ char, movies }) => {
+  const [open, SetOpen] = useState(false);
+  return (
+    <div
+      className={`flex flex-col border-2 border-white text-[rgb(255,255,255)] shadow-md bg-[black] mx-[25vw] px-[3vw] rounded max-h-[2.5vw] overflow-hidden ${
+        open ? "max-h-[30vw] my-[2vw] shadow-xl " : "my-[1vw] "
+      }`}
+    >
+      <div
+        className="flex justify-between "
+        onClick={() => {
+          SetOpen(!open);
+        }}
+      >
+        <h2 className="font-bold text-3xl flex justify-self-start  ">
+          {char.name}
+        </h2>
+        <div
+          className={` text-3xl ${
+            open ? "rotate-[270deg]" : "rotate-[90deg]"
+          } `}
+        >{`>`}</div>
+      </div>
+
+      <div className="flex transition-duration: 15000ms   flex-col items-center text-[1.5vw] ">
+        <ul className="flex flex-col items-start">
+          <li>gender: {char.gender}</li>
+          <li>height: {char.height}</li>
+          <li>mass: {char.mass} </li>
+          <li>birth year: {char.birth_year}</li>
+          <li>eye color: {char.eye_color}</li>
+          <li>appear in {char.films.length} films</li>
+          <li className="flex flex-col items-start">
+            movies :{" "}
+            {char.films.map((film, i) => (
+              <div key={i}>
+                {movies.find((m) => m.title === film)?.title}-
+                {movies.find((m) => m.title === film)?.director}
+              </div>
+            ))}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
